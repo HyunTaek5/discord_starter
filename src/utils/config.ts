8804a48baeda94d89a8b78f5914854936a74316a
HyunTaek Oh <oht222@naver.com>
2022-02-01 01:46:36 +0900

    config 관리 모듈 추가

---
+++ src/utils/config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    token: process.env.TOKEN, // 디스코드 봇 토큰
+    embed: {
+        color: "#FF0000",
+        footer: "discord-starter | Open source"
+    },
+    prefix: "w!",
+    clientId: process.env.CLIENT_ID, // 디스코드 클라이언트 Id
+};
